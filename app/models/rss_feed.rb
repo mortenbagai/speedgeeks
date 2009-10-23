e08d0d066acf87beaa5236ed1af7691627621b4e
Ricardo Chimal, Jr <ricardo@heroku.com>
2009-10-23 00:34:15 -0700

    added RSSFeed model

---
+++ app/models/rss_feed.rb
@@ -0,0 +1,25 @@
+class RSSFeed < ActiveRecord::Base
+	validates_uniqueness_of :url
+
+	class Unavailable < RuntimeError; end
+
+	def items
+		feed.xpath("//rss/channel/item").collect do |item|
+			{
+				:title => item.at("title").text.strip,
+				:link => item.at("link").text.strip
+			}
+		end
+	rescue Unavailable
+		[]
+	end
+
+	def feed
+		begin
+			res = RestClient.get(url)
+			Nokogiri.XML(res)
+		rescue Object => e
+			raise Unavailable, e.message
+		end
+	end
+end
