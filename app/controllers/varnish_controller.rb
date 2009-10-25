2c543265104c8cedab96a4ff8103d744e4405add
Morten Bagai <morten@heroku.com>
2009-10-25 11:06:32 -0700

    rename controllers

--- app/controllers/varnish_controller.rb
+++ app/controllers/varnish_controller.rb
@@ -6,19 +6,10 @@ class VarnishController < ApplicationController
 	end
 
 	def slow
-		render :text => "PI is #{Slow.calculate_pi} after 10 million iterations"
-	end
-
-	def fast
-		headers['Cache-Control'] = 'public; max-age=300'
-		render :text => "PI is #{Slow.calculate_pi} after 10 million iterations"
-	end
-
-	def img_slow
 		send_data Slow.rotate_and_resize, :filename => "jelly_fish.png", :type => "image/png", :disposition => 'inline'
 	end
 
-	def img_fast
+	def fast
 		headers['Cache-Control'] = 'public; max-age=2592000'
 		send_data Slow.rotate_and_resize, :filename => "jelly_fish.png", :type => "image/png", :disposition => 'inline'
 	end
