65af62f1565cc45720f5f2eb59d5c06fc9eb8728
Morten Bagai <morten@heroku.com>
2009-10-25 18:49:23 -0700

    default route is varnish_fast

--- config/routes.rb
+++ config/routes.rb
@@ -40,5 +40,5 @@ ActionController::Routing::Routes.draw do |map|
   # consider removing or commenting them out if you're using named routes and resources.
   map.connect ':controller/:action/:id'
   map.connect ':controller/:action/:id.:format'
-  map.connect '/', :controller => 'varnish'
+  map.connect '/', :controller => 'varnish_fast'
 end
